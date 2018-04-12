6728bccb02d322e892a58df4b10189ccd654aa5a
amro eldewiny <amroeldewiny@gmail.com>
2018-04-12 15:38:30 +0200

    add new api router

--- src/app/dashboard/home/home.component.ts
+++ src/app/dashboard/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../servies/auth-service/auth.service';
+
+import { ICustomer } from '../../interface/customer.interface';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  customers: ICustomer[];
+
+  constructor(public auth: AuthService) { }
 
   ngOnInit() {
+    if (localStorage.getItem('token')) {
+      this.getCustomers();
+    } else {
+      console.log('something wrong here');
+    }
+  }
+
+  getCustomers() {
+    this.auth.getData('/api/customers')
+      .subscribe((res: ICustomer[]) => {
+        console.log(res);
+        this.customers = res;
+      })
+  }
+
+  editCustomer(id) {
+    console.log(id);
+  }
+
+  deleteCustomer(id) {
+    console.log(id);
   }
 
 }
---
+++ src/app/interface/customer.interface.ts
@@ -0,0 +1,7 @@
+export interface ICustomer {
+    id: number
+    userID: number,
+    name: string,
+    email: string,
+    address: string
+}
